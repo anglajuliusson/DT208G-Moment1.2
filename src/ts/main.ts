a1cc9462a41ca03e4e1e37eb2d4be505e76c42b4
Ängla Juliusson <angla02@icloud.com>
2025-04-09 10:18:18 +0200

    Interface

---
+++ src/ts/main.ts
@@ -0,0 +1,7 @@
+// Interface som definierar strukturen för en kurs 
+interface CourseInfo { 
+    code: string; 
+    name: string; 
+    progression: 'A'| 'B'| 'C'; // Begränsad till 'A', 'B' eller 'C' 
+    syllabus: string; 
+} 
